edd8201437efac250b772bee97d790c9ee83c82a
jhvital <joao.vital94@gmail.com>
2018-05-07 16:19:09 +0100

    build and install scipy with openblas
    Commented code

--- README.txt
+++ README.txt
@@ -4,6 +4,6 @@ Microsoft Visual Studio Build Tools
 Usage:
 1. Open an Anaconda Prompt and activate the environment you want to install scipy with openBLAS.
 2. Change to the directory where the build-scipy-openblas.bat is and run the batch file. Just type build-scipy-openblas in the prompt.
-3. The batch will install the required packages to build and will pause before the build starts. Make sure you read the attention warning about adjusting the processor power to avoid overheating.
+3. The batch will install the required packages to build and will pause before the build starts. Make sure you read the attention warning about adjusting the processor power to avoid overheating. More info: https://docs.microsoft.com/en-us/windows-hardware/customize/power-settings/configure-processor-power-management-options
 4. Continue with the build.
 5. Check the installation by importing scipy and its submodules in a python script
--- build-scipy-openblas.bat
+++ build-scipy-openblas.bat
@@ -1,21 +1,35 @@
+:: This .bat file was created for an easy run. Note that all of the commands can be ran separately in the anaconda prompt.
 @echo off
 call conda config --add channels conda-forge
+:: Install required packages
 call conda install -y apipkg attrs colorama cython execnet more-itertools pluggy py pytest pytest-faulthandler pytest-forked pytest-xdist six
 call conda install -y blas numpy nose openblas
 call conda install -y freetype jpeg libpng libtiff olefile pillow tk zlib
+
+:: Copy required libraries to the directory where the building process will search
 call copy %CONDA_PREFIX%\Library\lib\openblas.lib %CONDA_PREFIX%\libs
 call copy %CONDA_PREFIX%\Library\bin\openblas.dll %CONDA_PREFIX%\DLLs
+
+:: Change directory to clone the scipy repository.
 call cd c:\
 if exist "c:\scipy" (call echo Warning: The folder scipy already exists, skipping the cloning process.) else (call git clone https://github.com/scipy/scipy.git)
-echo The scipy build file was created at c:\scipy
+echo NOTE: The scipy repoository was cloned to c:\scipy
 call cd scipy
 echo ATTENTION! The build is about to start. The building process takes a long time and puts a lot of stress on the CPU. In order to avoid overheating, set the maximum cpu frequency to a low value, for example 60 percent. You can do this under the advanced settings of your power plan, on the processor power management tab. By not doing this, you risk placing your machine under high temperature, leading to hardware damage. More info: https://docs.microsoft.com/en-us/windows-hardware/customize/power-settings/configure-processor-power-management-options
 pause
+
+:: Start the building process. Protected to avoid ovewritting a successful build.
 if exist "C:\scipy\*.whl" (call echo Warning: scipy was already built, skipping build process.) else (call %CONDA_PREFIX%\python.exe -m pip wheel -v -v -v .)
+
+:: Get the created wheel name
 call dir /b *.whl > tmp
 call set /p WHEEL_NAME=<tmp
-call del tmp
 call cd c:\ 
+
+:: Install the built wheel in the current environment
 call %CONDA_PREFIX%\python.exe -m pip install c:\scipy\%WHEEL_NAME%
+
+:: Clean up.
+call del tmp
 call set WHEEL_NAME=
 @echo on
