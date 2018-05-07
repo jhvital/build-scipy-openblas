c2959583e4ba58de0589dd938e1b452de2f815a2
jhvital <joao.vital94@gmail.com>
2018-05-07 17:56:13 +0100

    build and install scipy with openblas
    some packages were missing

--- README.txt
+++ README.txt
@@ -1,9 +1,16 @@
+This .bat file was created for an easy run. Note that all of the commands can be ran separately in the anaconda prompt.
+It clones the latest scipy source code and builds and installs a scipy wheel for the current conda environment.
+
 Requires:
-Microsoft Visual Studio Build Tools
+	1. Miniconda
+	   https://conda.io/miniconda.html
+	
+	2. Microsoft Visual Studio Build Tools:
+	   https://wiki.python.org/moin/WindowsCompilers
 
 Usage:
-1. Open an Anaconda Prompt and activate the environment you want to install scipy with openBLAS.
-2. Change to the directory where the build-scipy-openblas.bat is and run the batch file. Just type build-scipy-openblas in the prompt.
-3. The batch will install the required packages to build and will pause before the build starts. Make sure you read the attention warning about adjusting the processor power to avoid overheating. More info: https://docs.microsoft.com/en-us/windows-hardware/customize/power-settings/configure-processor-power-management-options
-4. Continue with the build.
-5. Check the installation by importing scipy and its submodules in a python script
+	1. Open an Anaconda Prompt and activate the environment you want to install scipy with openBLAS.
+	2. Change to the directory where the build-scipy-openblas.bat is and run the batch file. Just type build-scipy-openblas in the prompt.
+	3. The batch will install the required packages to build and will pause before the build starts. Make sure you read the attention warning about adjusting the processor power to avoid overheating. More info: https://docs.microsoft.com/en-us/windows-hardware/customize/power-settings/configure-processor-power-management-options
+	4. Continue with the build.
+	5. Check the installation by importing scipy and its submodules in a python script
--- build-scipy-openblas.bat
+++ build-scipy-openblas.bat
@@ -1,7 +1,9 @@
 :: This .bat file was created for an easy run. Note that all of the commands can be ran separately in the anaconda prompt.
 @echo off
 call conda config --add channels conda-forge
+
 :: Install required packages
+call conda install -y clangdev flang libflang llvm-meta openmp vc
 call conda install -y apipkg attrs colorama cython execnet more-itertools pluggy py pytest pytest-faulthandler pytest-forked pytest-xdist six
 call conda install -y blas numpy nose openblas
 call conda install -y freetype jpeg libpng libtiff olefile pillow tk zlib
@@ -11,11 +13,12 @@ call copy %CONDA_PREFIX%\Library\lib\openblas.lib %CONDA_PREFIX%\libs
 call copy %CONDA_PREFIX%\Library\bin\openblas.dll %CONDA_PREFIX%\DLLs
 
 :: Change directory to clone the scipy repository.
-call cd c:\
-if exist "c:\scipy" (call echo Warning: The folder scipy already exists, skipping the cloning process.) else (call git clone https://github.com/scipy/scipy.git)
-echo NOTE: The scipy repoository was cloned to c:\scipy
+call cd %CONDA_PREFIX%
+if exist "%CONDA_PREFIX%\scipy" (call echo Warning: The folder scipy already exists, skipping the cloning process.) else (call git clone https://github.com/scipy/scipy.git)
+echo NOTE: The scipy repository was cloned to %CONDA_PREFIX%\scipy
 call cd scipy
 echo ATTENTION! The build is about to start. The building process takes a long time and puts a lot of stress on the CPU. In order to avoid overheating, set the maximum cpu frequency to a low value, for example 60 percent. You can do this under the advanced settings of your power plan, on the processor power management tab. By not doing this, you risk placing your machine under high temperature, leading to hardware damage. More info: https://docs.microsoft.com/en-us/windows-hardware/customize/power-settings/configure-processor-power-management-options
+
 pause
 
 :: Start the building process. Protected to avoid ovewritting a successful build.
@@ -28,7 +31,7 @@ call cd c:\
 
 :: Install the built wheel in the current environment
 call %CONDA_PREFIX%\python.exe -m pip install c:\scipy\%WHEEL_NAME%
-
+copy c:\scipy\%WHEEL_NAME% %CONDA_PREFIX%\
 :: Clean up.
 call del tmp
 call set WHEEL_NAME=
