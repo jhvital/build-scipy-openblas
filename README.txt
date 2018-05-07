81d5da87c5d0adfc7b07fef1a33fab8480ffc894
jhvital <joao.vital94@gmail.com>
2018-05-07 15:32:00 +0100

    insert README
    Inserted README file with instructions and requisites.

---
+++ README.txt
@@ -0,0 +1,9 @@
+Requires:
+Microsoft Visual Studio Build Tools
+
+Usage:
+1. Open an Anaconda Prompt and activate the environment you want to install scipy with openBLAS.
+2. Change to the directory where the build-scipy-openblas.bat is and run the batch file. Just type build-scipy-openblas in the prompt.
+3. The batch will install the required packages to build and will pause before the build starts. Make sure you read the attention warning about adjusting the processor power to avoid overheating.
+4. Continue with the build.
+5. Check the installation by importing scipy and its submodules in a python script
--- build-scipy-openblas.bat
+++ build-scipy-openblas.bat
@@ -10,7 +10,6 @@ if exist "c:\scipy" (call echo Warning: The folder scipy already exists, skippin
 echo The scipy build file was created at c:\scipy
 call cd scipy
 echo ATTENTION! The build is about to start. The building process takes a long time and puts a lot of stress on the CPU. In order to avoid overheating, set the maximum cpu frequency to a low value, for example 60 percent. You can do this under the advanced settings of your power plan, on the processor power management tab. By not doing this, you risk placing your machine under high temperature, leading to hardware damage. More info: https://docs.microsoft.com/en-us/windows-hardware/customize/power-settings/configure-processor-power-management-options
-@echo on
 pause
 if exist "C:\scipy\*.whl" (call echo Warning: scipy was already built, skipping build process.) else (call %CONDA_PREFIX%\python.exe -m pip wheel -v -v -v .)
 call dir /b *.whl > tmp
@@ -19,3 +18,4 @@ call del tmp
 call cd c:\ 
 call %CONDA_PREFIX%\python.exe -m pip install c:\scipy\%WHEEL_NAME%
 call set WHEEL_NAME=
+@echo on
